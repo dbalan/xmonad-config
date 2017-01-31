8030eb4c0f4a41fe1dbd8923fd6021aae20fe1d3
Dhananjay Balan <dhananjay@cliqz.com>
2017-01-31 18:43:27 +0100

    Move to KDE
    Its actually a well developed environment. Everything is integrated...

--- xmonad-kde.hs
+++ xmonad-kde.hs
@@ -1,20 +1,49 @@
+{-
+Launch xmonad with KDE. Works with KDE5
+https://wiki.haskell.org/Xmonad/Using_xmonad_in_KDE
+-}
 import XMonad
 import XMonad.Config.Kde
 import qualified XMonad.StackSet as W -- to shift and float windows
- 
-main = xmonad kdeConfig
-    { modMask = mod4Mask -- use the Windows button as mod
-    , manageHook = manageHook kdeConfig <+> myManageHook
+import XMonad.Util.Run
+import XMonad.Util.EZConfig
+
+-- rewrite the start progs
+startProgs prgs = mapM_ (\(cmd,args) -> safeSpawn cmd args) prgs
+
+initProgs = [ ("setxkbmap", ["-option", "ctrl:nocaps"])
+            , ("emacs", ["--daemon"])
+            -- , ("kupfer", ["--no-splash"])
+            -- , ("diodon", [])
+            ]
+
+main = do
+  xmonad $ kdeConfig
+    { terminal    = "gnome-terminal"
+    , modMask     = mod4Mask
+    , focusFollowsMouse = False
+    , borderWidth = 3
+    , focusedBorderColor = "#e57a10"
+    , startupHook = do
+        startupHook kdeConfig
+        startProgs initProgs
+    -- WIP: not ready yet , manageHook = manageHook kdeConfig <+> customManageHook
     }
- 
-myManageHook = composeAll . concat $
+    `additionalKeysP`
+    [ ("M-x b", safeSpawn "firefox" [])
+    , ("M-x e", safeSpawn "emacsclient" ["-c"])
+    , ("M-p", safeSpawn "krunner" [])
+    ]
+
+
+customManageHook = composeAll . concat $
     [ [ className   =? c --> doFloat           | c <- myFloats]
     , [ title       =? t --> doFloat           | t <- myOtherFloats]
-    , [ className   =? c --> doF (W.shift "2") | c <- webApps]
-    , [ className   =? c --> doF (W.shift "3") | c <- ircApps]
+    , [ className   =? c --> doF (W.shift "1") | c <- webApps]
+    , [ className   =? c --> doF (W.shift "9") | c <- commApps]
     ]
   where myFloats      = ["MPlayer", "Gimp"]
         myOtherFloats = ["alsamixer"]
         webApps       = ["Firefox-bin", "Opera"] -- open on desktop 2
-        ircApps       = ["Ksirc"]                -- open on desktop 3
+        commApps       = ["KMail", "Slack - Cliqz"]  -- open on desktop 9
 
