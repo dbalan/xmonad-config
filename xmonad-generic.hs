c715384965f79328d24211020500152f8e09f0e1
Dhananjay Balan <mail@dbalan.in>
2018-05-27 15:17:37 +0530

    Enhance:  1. Smart Borders  2. goto-search-window menu.

--- xmonad-generic.hs
+++ xmonad-generic.hs
@@ -1,20 +1,32 @@
 import XMonad
 import XMonad.Util.Run
 import XMonad.Util.EZConfig
+import XMonad.Actions.WindowBringer
+import XMonad.Layout.NoBorders (smartBorders)
 
 -- rewrite the start progs
 startProgs prgs = mapM_ (\(cmd,args) -> safeSpawn cmd args) prgs
 
+initProgs = [ ("emacs", ["--daemon"])
+--            , ("xscreensaver", [])
+            ]
 main = do
   xmonad $ defaultConfig
     { terminal    = "sakura"
     , modMask     = mod4Mask
-    , focusFollowsMouse = False
+    , focusFollowsMouse = True
     , borderWidth = 1
+    , layoutHook = smartBorders $ layoutHook defaultConfig
+    , startupHook = do
+        startupHook defaultConfig
+        startProgs initProgs
     }
     `additionalKeysP`
     [ ("M-x b", safeSpawn "chrome" [])
     , ("M-x e", safeSpawn "emacsclient" ["-c"])
     , ("M-[", safeSpawn "dmenu_run" [])
+    -- window bringer actions
+    , ("M-S-g", gotoMenu)
+    , ("M-S-k", bringMenu)
     ]
 
