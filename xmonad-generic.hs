7d894d59b5d65ad1d9509e608a2821502b782d62
Dhananjay Balan <mail@dbalan.in>
2018-05-31 18:29:07 +0530

    layout changes:  1. Uses custom layout  2. avoidstruts param

--- xmonad-generic.hs
+++ xmonad-generic.hs
@@ -3,6 +3,8 @@ import XMonad.Util.Run
 import XMonad.Util.EZConfig
 import XMonad.Actions.WindowBringer
 import XMonad.Layout.NoBorders (smartBorders)
+import XMonad.Hooks.ManageDocks
+import XMonad.Layout.Tabbed (simpleTabbed)
 
 -- rewrite the start progs
 startProgs prgs = mapM_ (\(cmd,args) -> safeSpawn cmd args) prgs
@@ -10,13 +12,22 @@ startProgs prgs = mapM_ (\(cmd,args) -> safeSpawn cmd args) prgs
 initProgs = [ ("emacs", ["--daemon"])
             , ("xscreensaver", [])
             ]
+
+
+customLayout = avoidStruts ( tiled ||| Mirror tiled ||| Full ||| simpleTabbed ) ||| Full
+  where
+    tiled = Tall nmaster delta ratio
+    nmaster = 1
+    delta = 3/100
+    ratio = 1/2
+
 main = do
   xmonad $ defaultConfig
     { terminal    = "sakura"
     , modMask     = mod4Mask
     , focusFollowsMouse = True
     , borderWidth = 1
-    , layoutHook = smartBorders $ layoutHook defaultConfig
+    , layoutHook = smartBorders $ customLayout
     , startupHook = do
         startupHook defaultConfig
         startProgs initProgs
